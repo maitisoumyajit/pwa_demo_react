4d7f477e87752d4a9a70cd253b99316c8eda0764
wcj-cherry <cherrythejerry@gmail.com>
2024-09-10 22:01:54 +0530

    button added for test

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-        </a>
+        <a href="https://vitejs.dev" target="_blank"></a>
+        <a href="https://react.dev" target="_blank"></a>
       </div>
       <h1>Baba Bengali</h1>
       <div className="card">coming soon</div>
+      <button>hello</button>
     </>
   );
 }
