8c87118aa795bc88e7cdc656f31b1467a332bae1
wcj-cherry <cherrythejerry@gmail.com>
2024-10-05 01:21:48 +0530

    added bottom menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
+import BottomMenu from "./components/BottomMenu";
 
 function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank"></a>
-        <a href="https://react.dev" target="_blank"></a>
+        <BottomMenu/>
       </div>
       <h1>Baba Bengali</h1>
       <div className="card">coming soon</div>
---
+++ src/components/BottomMenu.tsx
@@ -0,0 +1,14 @@
+// BottomMenu.tsx
+import React from "react";
+
+const BottomMenu: React.FC = () => {
+  return (
+    <div className="bottom-menu">
+      <button className="menu-item">Home</button>
+      <button className="menu-item">Search</button>
+      <button className="menu-item">Profile</button>
+    </div>
+  );
+};
+
+export default BottomMenu;
