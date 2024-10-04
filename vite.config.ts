5f059c10a75087761bd186ccaa0cc5c3d8b17ff4
wcj-cherry <cherrythejerry@gmail.com>
2024-10-05 01:33:47 +0530

    zoom in mobile disabled, app name change

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       devOptions: {
         enabled: true,
       },
       manifest: {
-        name: ' PWA Demo',
-        short_name: 'PWA Demo',
-        description: 'A demo PWA built with React, Vite, and TypeScript',
+        name: "Baba Bengali",
+        short_name: "Baba Bengali",
+        description: "Split wise",
         start_url: "/",
         display: "standalone",
         background_color: "#ffffff",
-        theme_color: '#ffffff',
+        theme_color: "#ffffff",
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
         ],
       },
     }),
   ],
-})
+});
